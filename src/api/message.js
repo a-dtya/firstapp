fc4c90a953d583eae0d11329be9bccaa426ab42f
Adithya V <adithyavinod1943@gmail.com>
2023-07-17 06:58:56 +0530

    Added ScrollView

--- src/api/message.js
+++ src/api/message.js
@@ -1,10 +1,18 @@
-export default messages = [
+export default defaultMessages = [
     {
         role:'user',
-        context:'hello'
+        content:'hello'
     },
     {
-        role:'admin',
-        context:'this is ai'
+        role:'bot',
+        content:'hello! I\'m bonnie'
+    },
+    {
+        role:'user',
+        content: 'draw a picture'
+    },
+    {
+        role:'bot',
+        content:'https://cdn.pixabay.com/photo/2022/04/12/04/43/kiwi-7127148_1280.png'
     }
 ]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import { View, Text,SafeAreaView,Image } from 'react-native'
+import { View, Text,SafeAreaView,Image,ScrollView } from 'react-native'
 import React,{useState} from 'react'
+import defaultMessages from '../api//message'
 
 export default function HomeScreen() {
-  const [message,setMessage] = useState([])
+  const [message,setMessage] = useState(defaultMessages)
   return (
     <SafeAreaView className="flex-1 bg-yellow-100">
         {
-          message.length>0 ?(
+          message.length==0 ?(
             <View></View>
           ):(
             <>
@@ -19,8 +20,51 @@ export default function HomeScreen() {
             <View className="flex-row justify-center mt-3">
                 <Text className="text-black font-semibold text-2xl tracking-wide">Chat</Text>
               </View>
-            <View className="bg-neutral-200 rounded-2xl h-3/5 m-4">
+            <View className="bg-neutral-200 rounded-2xl h-3/6 m-4 p-2">
+                <ScrollView
+                  className="space-y-4"
+                  showsVerticalScrollIndicator={false}
+                >
+                  {
+                    message.map((msg,index)=>{
+                      if(msg.role == 'bot'){
+                        if(msg.content.includes('https')){
+                          return(
+                            <View key={index} className="flex-row justify-start">
+                              <View className="p-2 rounded-2xl bg-blue-100 rounded-tl-none">
+                                <Image className="rounded-2xl h-32 w-32"
+                                  source={{uri:msg.content}}
+                                  resizeMode='contain'
 
+                                ></Image>
+                              </View>
+                            </View>
+                          )
+                        }else{
+                          return(
+                            <View className="flex-row justify-start">
+                              <View className="bg-blue-100 rounded-xl rounded-tl-none p-2">
+                                <Text >
+                                  {msg.content}
+                                </Text>
+                              </View>
+                            </View>
+                          )
+                        }
+                      }else{
+                        return(
+                          <View className="flex-row justify-end">
+                            <View className="bg-blue-100 rounded-xl rounded-tr-none p-2">
+                              <Text>
+                                {msg.content}
+                              </Text>
+                            </View>
+                          </View>
+                        )
+                      }
+                    })
+                  }
+                </ScrollView>
             </View>  
               </>
           )
